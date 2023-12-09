66a5b1d3ef0a05a2abc4027af32fbca43e516db7
Priyobrato.Das <priyodas12@gmail.com>
2023-12-09 18:42:42 +0530

    structural directive:ngFor

--- src/app/learning/exm-structural-directive/exm-structural-directive.component.ts
+++ src/app/learning/exm-structural-directive/exm-structural-directive.component.ts
@@ -7,6 +7,32 @@ import { Component } from '@angular/core';
 })
 export class ExmStructuralDirectiveComponent {
   status: boolean = false;
+  productList: string[] = [
+    "product1", "product2", "product3", "product4", "product5", "product6"
+  ]
+
+  mobileList: any[] = [
+    {
+      name: "Nokia",
+      price: 10000
+    },
+    {
+      name: "Samsung",
+      price: 16000
+    },
+    {
+      name: "Apple",
+      price: 130000
+    },
+    {
+      name: "Oppo",
+      price: 80000
+    },
+    {
+      name: "1Plus",
+      price: 40000
+    }
+  ]
   onClickBtn() {
     console.log(Math.floor(Math.random() * 100));
     this.status = Math.floor(Math.random() * 100) % 2 === 0
