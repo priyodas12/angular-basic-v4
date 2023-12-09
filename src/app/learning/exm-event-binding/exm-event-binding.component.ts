bfa3a93b7d74da7ef4b9f0dec6face8dfada7cc4
Priyobrato.Das <priyodas12@gmail.com>
2023-12-09 11:46:00 +0530

    event binding example

--- src/app/learning/exm-event-binding/exm-event-binding.component.ts
+++ src/app/learning/exm-event-binding/exm-event-binding.component.ts
@@ -6,5 +6,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./exm-event-binding.component.css']
 })
 export class ExmEventBindingComponent {
+  count: number = 0;
+  inputText: string = "";
 
+  incrementTenHandler = () => {
+    this.count = this.count + 10;
+  }
+
+  decrementTenHandler = () => {
+    this.count = this.count - 10;
+  }
+
+  incrementFiveHandler = () => {
+    this.count = this.count + 5;
+  }
+
+  decrementFiveHandler = () => {
+    this.count = this.count - 5;
+  }
+
+  inputEventHandler(event: any) {
+    console.log(event);
+    this.inputText = (event.target as HTMLInputElement).value;
+  }
 }
